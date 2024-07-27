210a977cffc926bb8aa1a35aa8ae93ed98386d0c
Zakary Timson <zaktimson@gmail.com>
2024-07-26 23:11:35 -0400

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-dd if=/dev/zero of=/swapfile bs=1m count=1024
+SIZE="1024"
+if [ -n "$1" ]; then SIZE="$1"; fi
+
+dd if=/dev/zero of=/swapfile bs=1m count="$SIZE"
 chown 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
